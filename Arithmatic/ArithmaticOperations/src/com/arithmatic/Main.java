b685b03fe9e9fcd0a008a57466f1068525f0b124
AshishTone <ashishtone123@gmail.com>
2025-12-11 09:58:56 +0530

    adding Main & arithmatic classes! ✔

---
+++ Arithmatic/ArithmaticOperations/src/com/arithmatic/Main.java
@@ -0,0 +1,9 @@
+package com.arithmatic;
+
+public class Main {
+	public static void main(String args[]) {
+		arithmatic ath = new arithmatic();
+		System.out.println("Perimeter of 23x20 rectangle: " + ath.CalPeriRect(23, 20));
+	}
+
+}
